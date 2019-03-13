8bfa1b0e7c9b48bbe7979b65d97f78a145ef0b64
Neenu Kurian <nkurian@salmon.com>
2019-03-13 12:15:05 +0100

    nested class

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div >
-    <h1>{{ title }}</h1>
-    <ninjas></ninjas>
+  <div>
+    <app-header></app-header>
+    <app-ninjas></app-ninjas>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import Ninjas from './Ninjas.vue'
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Ninjas from "./components/Ninjas.vue";
 
 export default {
-  components:{
-    'ninjas':Ninjas
+  components: {
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-ninjas": Ninjas
   },
-  data () {
-    return {
-      title: 'Ninja App'
-    }
+  data() {
+    return {};
   }
-}
+};
 </script>
 
-<style scoped>
-  h1{
-    color:blueviolet;
-  }
-
+<style>
 </style>
