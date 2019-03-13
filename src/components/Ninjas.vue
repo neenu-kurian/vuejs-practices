b21d41bbdd018d40a54f63546ea162842af166f3
Neenu Kurian <nkurian@salmon.com>
2019-03-13 16:17:43 +0100

    lifecycle

--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -30,6 +30,25 @@ export default {
       deleteNinja:function() {
           this.ninjas.pop();
       }
+  },
+  //lifecycle hook 
+  beforeCreate() {
+      alert('beforeCreate');
+  },
+  created() {
+      alert('created');
+  },
+  beforeMount() {
+      alert ('beforeMount');
+  },
+  mounted() {
+      alert ('mounted');
+  },
+  beforeUpdate() {
+      alert('beforeUpdate');
+  },
+  updated() {
+      alert('updated');
   }
 }
 </script>
